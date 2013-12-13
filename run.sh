6108047ff98b639987b938c113bb535983386d4b
Jonathan Kim <me@jonathan-kim.com>
2013-12-13 13:49:26 -0500

    Fixing.

--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@ if [ "$WERCKER_RESULT" = "passed" ]; then
     git config --global user.email pleasemailus@wercker.com
     git config --global user.name "wercker"
 
-    # Tag your commit.
+    # Get tags.
+    git fetch --tags $GIT_REMOTE
+
+    # Delete existing commit.
+    # TODO: Handle first-time tagging. For now, you have to manually push the tags
+    # before using this step.
     git tag -d deploy-$WERCKER_DEPLOYTARGET_NAME
+
+    # Tag your commit.
     git tag -a deploy-$WERCKER_DEPLOYTARGET_NAME $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
     git push --tags $GIT_REMOTE
 fi
