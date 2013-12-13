81b26bd47f1c0bb48d100fa9c67b593ec05be488
Jonathan Kim <me@jonathan-kim.com>
2013-12-13 11:10:10 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wercker Git Tag Step
+
+Updates git tags so you know what's currently deployed.
+
+## Options
+
+Step has no option.
+
+## Example
+
+    - git-tag
---
+++ run.sh
@@ -0,0 +1,8 @@
+# Configure Git.
+git config --global user.email pleasemailus@wercker.com
+git config --global user.name "wercker"
+
+# Tag your commit.
+git tag -d deploy-$WERCKER_DEPLOYTARGET_NAME
+git tag -a deploy-$WERCKER_DEPLOYTARGET_NAME $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
+git push --tags $GIT_REMOTE
