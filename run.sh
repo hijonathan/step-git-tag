b1aca7c5984ebdf9234affa5d4b6eca5b1fc73a0
Jonathan Kim <me@jonathan-kim.com>
2013-12-13 13:32:03 -0500

    Only push git tags when build is successful.

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 # Configure Git.
-git config --global user.email pleasemailus@wercker.com
-git config --global user.name "wercker"
+if [ "$WERCKER_RESULT" = "passed" ]; then
+    git config --global user.email pleasemailus@wercker.com
+    git config --global user.name "wercker"
 
-# Tag your commit.
-git tag -d deploy-$WERCKER_DEPLOYTARGET_NAME
-git tag -a deploy-$WERCKER_DEPLOYTARGET_NAME $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
-git push --tags $GIT_REMOTE
+    # Tag your commit.
+    git tag -d deploy-$WERCKER_DEPLOYTARGET_NAME
+    git tag -a deploy-$WERCKER_DEPLOYTARGET_NAME $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
+    git push --tags $GIT_REMOTE
+fi
