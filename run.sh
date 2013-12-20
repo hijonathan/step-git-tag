9d01f053422f82c8642a8ec62ff667e7e4c4b3b1
Pieter Joost van de Sande <pj@born2code.net>
2013-12-20 15:38:04 +0100

    Skip git tag removal if the tag does not exist

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 # Configure Git.
 if [ "$WERCKER_RESULT" = "passed" ]; then
+    # Configure git
     git config --global user.email pleasemailus@wercker.com
     git config --global user.name "wercker"
 
     # Get tags.
     git fetch --tags $GIT_REMOTE
 
-    # Delete existing commit.
-    # TODO: Handle first-time tagging. For now, you have to manually push the tags
-    # before using this step.
-    git tag -d deploy-$WERCKER_DEPLOYTARGET_NAME
+    # Create the name of the tag
+    tagname="deploy-$WERCKER_DEPLOYTARGET_NAME"
+    
+    # Delete the tag if it exists, otherwise just skip
+    (git tag -l | grep "$tagname") && git tag -d "$tagname" 
 
     # Tag your commit.
     git tag -a deploy-$WERCKER_DEPLOYTARGET_NAME $WERCKER_GIT_COMMIT -m "Wercker deploy by $WERCKER_STARTED_BY :articulated_lorry:."
